e3f6665ea804a1b46319d1f6e6917cdb75d8c84c
kasloww <kdavidson@brooklynsteamcenter.org>
2023-09-19 10:47:53 -0400

    same but done

--- benchmark.py
+++ benchmark.py
@@ -2,11 +2,26 @@
 print("Kasim")
 # Exercise 2 
 favoriteShow = "Suits"
+# Exercise 3
+height = '6.1'
+age = 17
+print (height * age)
 # Exercise 4
 print("im going to break")
 # Exercise 5
-fav_food = "Shrimp Alfredo" "Calamari" "Pancakes"
+fav_food = ["Shrimp Alfredo" , "Calamari" , "Pancakes"]
+# Exercise 6 
+if age >= 21:
+    print('You can rent a car')
+else:
+    print('Your beat')
 # Exercise 7
 name = input("What is your name? ")
-print("Hello," + name ".")
+print("Hello" + name)
+# Exercise 8 
+for k in fav_food:
+    print(k)
 # Exercise 9 
+x = 1
+while x == 2:
+    print("no") 
