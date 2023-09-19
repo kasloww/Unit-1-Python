ab48c44bebd31f417e65410a9a359f125aee9365
kasloww <kdavidson@brooklynsteamcenter.org>
2023-09-19 09:50:53 -0400

    exercises

---
+++ benchmark.py
@@ -0,0 +1,12 @@
+# Exercise 1
+print("Kasim")
+# Exercise 2 
+favoriteShow = "Suits"
+# Exercise 4
+print("im going to break")
+# Exercise 5
+fav_food = "Shrimp Alfredo" "Calamari" "Pancakes"
+# Exercise 7
+name = input("What is your name? ")
+print("Hello," + name ".")
+# Exercise 9 
