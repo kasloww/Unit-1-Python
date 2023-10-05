9c6e9c5c4c29691024bcd737167cde71d7cdeac6
kasloww <kdavidson@brooklynsteamcenter.org>
2023-10-05 10:53:14 -0400

    not done :(

---
+++ flipping_for.py
@@ -0,0 +1,35 @@
+"""
+Exercise 1:
+Write a program to print each character of a given string using a for loop.
+"""
+nah = "string"
+for a in nah:
+    print(a)
+# The amount of times the variable printed was becuse of how much letters were in the string so yes I did expect it
+# made a variable and assigned a value to it then another varible being made because of the for loop and prints the amount of value the variable has to it
+"""
+Exercise 2:
+Write a program to calculate the sum of elements in a list using a for loop.
+"""
+add = [7,17,18,21]
+sum = 0
+for num in add:
+    sum += num
+    print("The sum of the list is:", sum)
+# I didn't expect it to come out like this, I thought it was going to add up every number together not every other number
+# First variable holds all numbers and the sum += num code makes every number add onto each other
+"""
+Exercise 3:
+Write a program to find and print the length of each word in a sentence using a for loop and a list.
+""
+
+Excercise 4:
+Write a program that creates a dictionary (atleast 4 key:value pairs) and then
+iterates through a dictionary and prints the result
+"""
+diction = {
+    'pencils': 1,
+    'bags': 2,
+    'shoes': 3,
+    
+}
