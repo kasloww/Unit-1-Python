ee32f327e08eb3bfe70c9473805a744005b1e1ce
kasloww <kdavidson@brooklynsteamcenter.org>
2023-10-12 10:45:14 -0400

    functions woohoo

---
+++ fab_functions.py
@@ -0,0 +1,44 @@
+# Task 1: Greeting Function
+# Write a function `greet(name)` that takes a name as a parameter and prints a greeting message like "Hello, [name]!".
+def greet(sir):
+    # defined a function, made a varible name
+    print("Hello, " + sir)
+
+greet("Kas!")
+
+# print statement value for the variable sir
+
+# Task 2: Sum of Two Numbers
+# Write a function `sum_numbers(a, b)` that takes two numbers as parameters and returns their sum.
+def sum_numbers(sume):
+    print(sume)
+    
+sum_numbers(1 + 3)
+
+# the value of the variable for the function is the numbers being added then printing the sum
+
+# Task 3: Calculate Factorial
+# Write a function `factorial(n)` that calculates the factorial of a given number `n`.
+import math
+
+def factorial(nahh):
+    print(math.factorial(nahh))
+    # imported math so I can use factorial statement
+factorial(718)
+# Task 4: Check Even or Odd
+# Write a function `is_even(num)` that takes a number as a parameter and returns `True` if the number is even, and `False` otherwise.
+def is_even(odde):
+    print('False')
+
+is_even(7)
+
+# 7 is odd so printed false with the function
+
+# Task 5: Calculate Area of a Rectangle
+# Write a function `calculate_area(length, width)` that calculates and returns the area of a rectangle given its length and width.
+def calculate_area(lew):
+    print(lew)
+
+calculate_area(3 * 7)
+
+# area is length times width
