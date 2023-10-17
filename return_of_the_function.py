531846569a3cbb9316c48b2e60bb0eee030ccb41
kasloww <kdavidson@brooklynsteamcenter.org>
2023-10-17 10:51:46 -0400

    kept trying lol

---
+++ return_of_the_function.py
@@ -0,0 +1,26 @@
+"""
+Task 1: Calculate the Square of a Number
+Write a function that takes an integer as an argument and returns its square.
+"""
+
+"""
+Task 2: Calculate the Area of a Rectangle:
+Write a function that takes the length and width of a rectangle and returns its area.
+"""
+def lew(a, b):
+    return a * b
+
+c = lew(6, 10)
+print(c)
+"""
+Task 3: Convert Temperature from Celsius to Fahrenheit:
+Write a function that takes a temperature in Celsius and returns 
+the equivalent temperature in Fahrenheit using the correct formula
+"""
+
+
+"""
+Task 4: Calculate the Average of Numbers:
+Write a function that takes a list of numbers 
+and returns the average (mean) of those numbers.
+"""
---
+++ try_except.py
@@ -0,0 +1,17 @@
+try:
+    age = int(input('Enter your age: '))
+except:
+   print("Type out the actual number")
+
+try: 
+    faveNum = int(input('What is your favorite number: '))
+except:
+   ("Don't put 0, 0 can't be divided.")
+if age <= 21:
+    print('You are not allowed to enter, you are too young.')
+else:
+    print('Welcome, you are old enough.')
+
+print("Type out the actual number instead of letters.")
+
+print("Fun Fact! Your age divided by your favorite number is: " , age / faveNum)
